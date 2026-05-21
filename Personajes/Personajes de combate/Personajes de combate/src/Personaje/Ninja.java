be4ea7d99d08c89a35650d27aa026bfad6d02260
davuggdeat <119799176+davuggdeat@users.noreply.github.com>
2026-05-21 08:41:03 -0300

    Update Ninja class with new attack logic and stats

--- Personajes/Personajes de combate/Personajes de combate/src/Personaje/Ninja.java
+++ Personajes/Personajes de combate/Personajes de combate/src/Personaje/Ninja.java
@@ -1,16 +1,24 @@
 package Personaje;
 
-public class Ninja extends Personaje{
+public class Ninja extends Personaje {
 
-	public Ninja(String nombre) {
-	    super(nombre, 100, 19, 8);
+public Ninja(String nombre) {
+    super(nombre, 100, 21, 10);
+    
+}
 
-      }
+public void DisparoDoble(Personaje enemigo) {
+System.out.println(getNombre() + " usa DisparoDoble.");
+enemigo.recibirDaño(25);
 
-	public void DisparoDoble(Personaje enemigo) {
-		System.out.println(getNombre() + " usa DisparoDoble.");
-		enemigo.recibirDaño(27);
-      }
+}
 
-   }
-	
+@Override
+public void atacar(Personaje enemigo) {
+    int daño = getAtaque() - enemigo.getDefensa();
+    if (daño < 0) daño = 0;
+    
+    enemigo.recibirDaño(daño);
+    System.out.println(getNombre() + " ataca sigilosamente causando  " + daño + " de daño.");
+}
+}
