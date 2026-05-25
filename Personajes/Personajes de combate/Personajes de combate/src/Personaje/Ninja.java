efaf7edf085762e7e885fd6d32c1447a802620b8
davuggdeat <119799176+davuggdeat@users.noreply.github.com>
2026-05-24 22:42:34 -0300

    Update Ninja.java

--- Personajes/Personajes de combate/Personajes de combate/src/Personaje/Ninja.java
+++ Personajes/Personajes de combate/Personajes de combate/src/Personaje/Ninja.java
@@ -1,24 +1,45 @@
 package Personaje;
 
 public class Ninja extends Personaje {
+	
+	private boolean escondido = false;
 
-public Ninja(String nombre) {
-    super(nombre, 100, 21, 10);
+    public Ninja(String nombre) {
+        super(nombre, 100, 23, 10);
+    }
     
-}
-
-public void DisparoDoble(Personaje enemigo) {
-System.out.println(getNombre() + " usa DisparoDoble.");
-enemigo.recibirDaño(25);
-
-}
-
-@Override
-public void atacar(Personaje enemigo) {
-    int daño = getAtaque() - enemigo.getDefensa();
-    if (daño < 0) daño = 0;
     
-    enemigo.recibirDaño(daño);
-    System.out.println(getNombre() + " ataca sigilosamente causando  " + daño + " de daño.");
-}
+    public void DisparoDoble(Personaje enemigo) {
+
+        System.out.println(getNombre() + " usa Disparo Doble.");
+
+        enemigo.recibirDaño(31);
+
+        System.out.println("¡Causó 31 de daño!");
+    }
+    
+    
+ // NUEVA HABILIDAD (Evadiendo el ataque)
+    public void esconderse(Personaje enemigo) {
+
+        escondido = true;
+
+        System.out.println(getNombre() + " se escondió entre las sombras.");
+        
+        int daño = 18;
+
+        
+		enemigo.recibirDaño(daño);
+
+        System.out.println(getNombre()  + " atacó desde las sombras causando " + daño + " de daño.");
+    }
+
+    public boolean estaEscondido() {
+        return escondido;
+    }
+
+    public void salirEscondite() {
+        escondido = false;
+    }
+		
 }
