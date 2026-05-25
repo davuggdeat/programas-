3d937840cdc12d2dfac9af9236e2957f9229d3ee
santiagonicolaslimaruiz-cmyk <santiagonicolaslimaruiz@gmail.com>
2026-05-25 10:16:23 -0300

    Update Personaje.java

--- Personajes/Personajes de combate/src/Personaje/Personaje.java
+++ Personajes/Personajes de combate/src/Personaje/Personaje.java
@@ -17,16 +17,47 @@ public class Personaje {
 	public void atacar(Personaje enemigo) {
 		int daño = ataque - enemigo.defensa;
 		
-		if (daño < 0) {
-			daño = 0;
-			
-	    }
-		
+		if (daño < 0) 
+			daño = 0;	
 		enemigo.recibirDaño(daño);
-		System.out.println(nombre + "atacó a " + enemigo.nombre + "causando" + daño + " de  daño.");
-	}
+		System.out.println(nombre + " ataco a " + enemigo.nombre + " causando " + daño + " de  daño.");
+		
+		}
 	
 	public void recibirDaño(int daño) {
 		vida = vida - daño;
+		
+		if (vida < 0) 
+			vida = 0;
+		}
+	
+	
+	public void mostrarEstado() {
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Vida: " + vida);
+		System.out.println("Ataque: " + ataque);
+		System.out.println("Defensa: " + defensa);
+		System.out.println("-----------------------");
 	}
+	
+	public boolean estaVivo() {
+	return vida > 0;
+	
+	}
+
+    public String getNombre() {
+    	return nombre;
+    }
+    
+    public int getVida() { 
+    	return vida;
+    }
+    
+    public int getAtaque() {
+    	return ataque;
+    }
+    
+    public int getDefensa() { 
+    	return defensa; 
+    }
 }
