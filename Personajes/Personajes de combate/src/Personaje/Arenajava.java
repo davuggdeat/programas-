02aa190a9e082f302e0431dbacd406b375b6be83
santiagonicolaslimaruiz-cmyk <santiagonicolaslimaruiz@gmail.com>
2026-05-25 10:14:54 -0300

    Update Arenajava.java

--- Personajes/Personajes de combate/src/Personaje/Arenajava.java
+++ Personajes/Personajes de combate/src/Personaje/Arenajava.java
@@ -2,32 +2,43 @@ package Personaje;
 
 public class Arenajava {
 
-	public static void main (String[]args) {
+	public static void main(String[] args) {
 		
-		Personaje jugador1 = new Personaje("Guerrero Azul", 100, 25, 8);
-		Personaje jugador2 = new Personaje("mago rojo", 100, 22, 5);
+		Ninja ninja = new Ninja("Shirai Ryu");
+        Guerrero guerrero = new Guerrero("Thor");
 		
 		System.out.println("Comienza la batalla en la Arena Java");
 		System.out.println();
 		
-		jugador1.mostrarEstado();
-		jugador2.mostrarestado();
-		
-		while (jugador1.estaVivo() && jugador2.estavivo()) {
-			   jugador1.atacar(jugador2);
-			
-			if(jugador2.estaVido()) {
-				jugador2.atacar(jugador1);
+		ninja.mostrarEstado();
+        guerrero.mostrarEstado();
+        
+		while (ninja.estaVivo() && guerrero.estaVivo()) {
+
+			double azar = Math.random();
+
+		     if (azar < 0.4) {
+
+				ninja.DisparoDoble(guerrero);
+
+			} else {
+
+				ninja.atacar(guerrero);
 			}
-			
-			jugador1.mostrarEstado();
-			jugador2.mostrarEstado();
-		}
+
+			if (guerrero.estaVivo()) {           
+				guerrero.atacar(ninja);
+			}
 		
-		if (jugador1.EstaVivo()) {
-			 System.out.println("Gano" + jugador1.getNombre());
+		
+			ninja.mostrarEstado();
+			guerrero.mostrarEstado();
+		
+		}
+		if (ninja.estaVivo()) {
+			System.out.println("Ganó " + ninja.getNombre());
 		} else {
-			System.out.println("Gano" + jugador2.getNombre());
+			System.out.println("Ganó " + guerrero.getNombre());
 		}
 	}
-}
+	}
